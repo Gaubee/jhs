261a0e66b94d634fe3175e0a0336ea7199daeac1
Gaubee <gaubeebangeel@gmail.com>
2015-08-20 13:45:42 +0800

    更改了产品目录

--- dotnar/product.config.js
+++ dotnar/product.config.js
@@ -4,9 +4,9 @@ module.exports = {
 	"www_root": "/usr/share/nginx/www/dotnar/public",
 	"admin_root": "/usr/share/nginx/www/dotnarAdmin/public",
 
-	"bus_root": "/usr/share/nginx/www/dotnar_base",
-	"default_mobile_template_root": "/usr/share/nginx/www/NEW_VERSION_DOTNAR_TEMPLATE/base_version",
-	"default_pc_template_root": "/usr/share/nginx/www/NEW_VERSION_DOTNAR_TEMPLATE/pc_base_version",
+	"bus_root": "/root/gitDepot/dotnar_base",
+	"default_mobile_template_root": "/root/gitDepot/base_version",
+	"default_pc_template_root": "/root/gitDepot/pc_base_version",
 
 	"lib_root": "/usr/share/nginx/www/O2O_front_end_lib",
 
