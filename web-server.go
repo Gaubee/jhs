fc15fd080afada9bb33a649626e7df6fd6a1de98
Gaubee <gaubeebangeel@gmail.com>
2016-03-15 15:29:05 +0800

    Signed-off-by: Gaubee <gaubeebangeel@gmail.com>

---
+++ web-server.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"io"
+	"net/http"
+)
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "Hello world!")
+}
+
+func qaq(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "Hello QAQ!")
+}
+
+var mux map[string]func(http.ResponseWriter, *http.Request)
+
+func main() {
+	server := http.Server{
+		Addr:    ":8000",
+		Handler: &myHandler{},
+	}
+
+	mux = make(map[string]func(http.ResponseWriter, *http.Request))
+	mux["/"] = hello
+	mux["/qaq"] = qaq
+
+	server.ListenAndServe()
+}
+
+type myHandler struct{}
+
+func (*myHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if h, ok := mux[r.URL.String()]; ok {
+		h(w, r)
+		return
+	}
+
+	io.WriteString(w, "My server: "+r.URL.String())
+}
